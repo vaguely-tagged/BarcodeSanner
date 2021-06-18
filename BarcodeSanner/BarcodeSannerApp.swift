b323b8694f07b34d90b5f1265c1b2c1c8bca729a
vaguely-tagged <sgherzivincent@gmail.com>
2021-06-18 18:17:29 +0900

    Initial Commit

---
+++ BarcodeSanner/BarcodeSannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeSannerApp.swift
+//  BarcodeSanner
+//
+//  Created by Vince Sgherzi on 6/18/21.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeSannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
